f378383e6beb766d1df0ac181478f6103a450a35
Aimable Ruhumuliza <phyntom@gmail.com>
2020-09-14 16:48:55 +0200

    added heal and attack feature

--- app.js
+++ app.js
@@ -6,9 +6,51 @@ new Vue({
       gameIsRunning: false,
    },
    methods: {
-      startGame() {
+      startGame: function () {
          this.gameIsRunning = true;
          this.playerHealth = 100;
          this.monsterHealth = 100;
       },
+      attack: function () {
+         this.monsterHealth -= this.computeDamage(3, 10);
+         if (this.checkWinner()) {
+            return;
+         }
+         this.playerHealth -= this.computeDamage(5, 12);
+         this.checkWinner();
+      },
+      heavyAttack: function () {},
+      heal: function () {
+         this.playerHealth += 5;
+      },
+      giveUp: function () {
+         this.gameIsRunning = false;
+         this.playerHealth = 100;
+         this.monsterHealth = 100;
+      },
+
+      computeDamage: function (min, max) {
+         let damage = Math.max(Math.floor(Math.random() * max + 1), min);
+         return damage;
+      },
+      checkWinner: function () {
+         if (this.monsterHealth <= 0) {
+            if (confirm('You won ! Wanna new game ?')) {
+               this.startGame();
+            } else {
+               this.gameIsRunning = false;
+            }
+            return true;
+         }
+         if (this.playerHealth <= 0) {
+            if (confirm('You lost ! Wanna new game ?')) {
+               this.startGame();
+            } else {
+               this.gameIsRunning = false;
+            }
+            return true;
+         }
+         return false;
+      },
    },
+});
