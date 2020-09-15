f0a9a16c083281994edf6dd3f34e83ac109a58a3
Aimable Ruhumuliza <phyntom@gmail.com>
2020-09-15 07:47:27 +0200

    added checkWinner method,heavyAttacks and log turns

--- app.js
+++ app.js
@@ -1,34 +1,69 @@
 new Vue({
    el: '#app',
    data: {
-      playerHealth: 80,
-      monsterHealth: 80,
+      playerHealth: 100,
+      monsterHealth: 100,
       gameIsRunning: false,
+      turns: [],
    },
    methods: {
       startGame: function () {
          this.gameIsRunning = true;
          this.playerHealth = 100;
          this.monsterHealth = 100;
+         this.turns = [];
       },
       attack: function () {
-         this.monsterHealth -= this.computeDamage(3, 10);
+         let damage = this.computeDamage(3, 10);
+         console.log(damage);
+         this.monsterHealth -= damage;
+         this.turns.unshift({
+            isPlayer: true,
+            text: `Player hits Monster for ` + damage,
+         });
          if (this.checkWinner()) {
             return;
          }
-         this.playerHealth -= this.computeDamage(5, 12);
-         this.checkWinner();
+         this.monsterAttacks();
       },
-      heavyAttack: function () {},
+      heavyAttack: function () {
+         let damage = this.computeDamage(10, 20);
+         this.monsterHealth -= damage;
+         this.turns.unshift({
+            isPlayer: true,
+            text: `Player hits Monster hard for ` + damage,
+         });
+         if (this.checkWinner()) {
+            return;
+         }
+         this.monsterAttacks();
+      },
       heal: function () {
-         this.playerHealth += 5;
+         if (this.playerHealth <= 10) {
+            this.playerHealth += 10;
+         } else {
+            this.playerHealth = 100;
+         }
+         this.turns.unshift({
+            isPlayer: true,
+            text: `Player heals by ` + 10,
+         });
+         this.monsterAttacks();
       },
       giveUp: function () {
          this.gameIsRunning = false;
-         this.playerHealth = 100;
-         this.monsterHealth = 100;
+         // this.playerHealth = 100;
+         // this.monsterHealth = 100;
       },
-
+      monsterAttacks: function () {
+         let damage = this.computeDamage(5, 12);
+         this.playerHealth -= damage;
+         this.turns.unshift({
+            isPlayer: false,
+            text: `Monster hits Player for ` + damage,
+         });
+         this.checkWinner();
+      },
       computeDamage: function (min, max) {
          let damage = Math.max(Math.floor(Math.random() * max + 1), min);
          return damage;
